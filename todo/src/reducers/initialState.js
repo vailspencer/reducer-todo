e97a5d02528384c9e31f1207665f3730446489b2
Spencer_Vail <vail.spencer@gmail.com>
2020-05-11 15:12:53 -0700

    initial commit

---
+++ todo/src/reducers/initialState.js
@@ -0,0 +1,15 @@
+import React, {useState, useReducer} from 'react'
+
+
+
+const initialValue = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+    }
+
+    const reducer = (state,action) => {
+
+
+        return state;
+}
